6a00272b907dae1c8e5e5db6c4c926c6045d8ad9
Anis Afifi <anis@afifi.pro>
2025-01-31 21:18:44 +0600

    12

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'tsup';
- 
+
 export default defineConfig({
     format: ['cjs', 'esm'],
     entry: ['./src/index.ts'],
@@ -7,4 +7,18 @@ export default defineConfig({
     shims: true,
     skipNodeModulesBundle: true,
     clean: true,
+    minify: 'terser',
+    terserOptions: {
+        mangle: {
+            toplevel: true,
+        },
+        compress: {
+            drop_console: true,
+            drop_debugger: true,
+            passes: 2,
+        },
+        output: {
+            comments: false,
+        },
+    },
 });
